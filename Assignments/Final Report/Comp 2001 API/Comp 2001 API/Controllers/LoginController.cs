0a35df3ea3f705c2f58c05e8fa2b58f3017fb03c
Zedster82 <55487742+Zedster82@users.noreply.github.com>
2024-01-12 02:51:35 +0000

    Get data from database with current logged in user credentials

--- Assignments/Final Report/Comp 2001 API/Comp 2001 API/Controllers/LoginController.cs
+++ Assignments/Final Report/Comp 2001 API/Comp 2001 API/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
 
 
 
@@ -20,10 +22,68 @@ namespace Comp_2001_API.Controllers
 
         // GET api/Login/username,password
         [HttpGet("{email},{password}")]
-        public Task<string> Get(string email,string password)
+        public string Get(string email,string password)
         {
 
-            return Login.LoginAuth(email, password);
+            Login.LoginAuth(email, password);
+
+            if (!Login.isLoggedIn)
+            {
+                return "Incorrect Login";
+            }
+
+
+
+            string connectionString = Configuration.GetConnectionString("Default");
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = $"SELECT * FROM CW2.[User] WHERE email=@Email";
+
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Email", email);
+                    try
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+
+                            //Read all data, using a data table to convert it
+                            var dataTable = new System.Data.DataTable();
+                            dataTable.Load(reader);
+
+                            string jsonConverted = JsonConvert.SerializeObject(dataTable);
+                            
+                            Login.accountID = Convert.ToInt32(dataTable.Rows[0]["user_id"].ToString());
+                            Login.username = dataTable.Rows[0]["username"].ToString();
+                            Login.email = dataTable.Rows[0]["email"].ToString();
+                            Login.password = dataTable.Rows[0]["password"].ToString();
+                            Login.accountType = dataTable.Rows[0]["account_type"].ToString();
+
+
+                            
+
+                            return $"Successfully logged in as user: {Login.username} with email of: {Login.email}";
+                            
+                            
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        return ex.Message;
+                    }
+                }
+            }
+
+
+
+
+
+            return "failed";
 
         }
 
